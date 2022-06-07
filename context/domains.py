547b8c65c10b1d0b152bcd9eed739ff84565bf7d
seunghyun <suji1541@gmail.com>
2022-06-07 18:04:13 +0900

    2022 06 07 18:00 grade ozone 연습

--- context/domains.py
+++ context/domains.py
@@ -1,54 +1,120 @@
 # context, fname, train, test, id , label
 # class Dataset:
+from abc import ABC
 from dataclasses import dataclass
+from abc import *
 
 
 @dataclass
 class Dataset:
-    context : str
-    sname : str
-    dname : str
-    fname : str
-    train : str
-    test : str
-    id : str
-    label : str
+    context: str
+    sname: str
+    dname: str
+    fname: str
+    train: str
+    test: str
+    id: str
+    label: str
 
     @property
-    def dname(self) -> str : return self._dname
+    def dname(self) -> str: return self._dname
+
     @dname.setter
-    def dname(self, dname) : self._dname = dname
+    def dname(self, dname): self._dname = dname
+
     @property
     def sname(self) -> str: return self._sname
+
     @sname.setter
     def sname(self, sname): self._sname = sname
 
     @property
-    def context(self) -> str : return self._context
+    def context(self) -> str: return self._context
+
     @context.setter
-    def context(self, context) : self._context = context
+    def context(self, context): self._context = context
 
     @property
     def fname(self) -> str: return self._fname
+
     @fname.setter
     def fname(self, fname): self._fname = fname
 
     @property
     def train(self) -> str: return self._train
+
     @train.setter
     def train(self, train): self._train = train
 
     @property
     def test(self) -> str: return self._test
+
     @test.setter
     def test(self, test): self._test = test
 
     @property
     def id(self) -> str: return self._id
+
     @id.setter
     def id(self, id): self._id = id
 
     @property
     def label(self) -> str: return self._label
+
     @label.setter
-    def label(self,label): self._label = label
+    def label(self, label): self._label = label
+
+
+class PrinterBase(metaclass=ABCMeta):  # abc 모듈을 가지고 오면 추상클래스
+    @abstractmethod
+    def dframe(self):
+        pass
+    # new file, csv, xls, json
+
+
+class ReaderBase(metaclass=ABCMeta):
+    @abstractmethod
+    def new_file(self):
+        pass
+
+    @abstractmethod
+    def csv(self):
+        pass
+
+    @abstractmethod
+    def xls(self):
+        pass
+
+    @abstractmethod
+    def json(self):
+        pass
+
+
+# Reader
+class Reader(ReaderBase):
+    def new_file(self):
+        pass
+
+    def csv(self):
+        pass
+
+    def xls(self):
+        pass
+
+    def json(self):
+        pass
+
+
+# Printer
+class Printer(PrinterBase):
+    def new_file(self):
+        pass
+
+    def csv(self):
+        pass
+
+    def xls(self):
+        pass
+
+    def json(self):
+        pass
