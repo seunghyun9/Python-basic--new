cd9f1deb8d1d470459bd242e4df4c2a59277b6ab
seunghyun <suji1541@gmail.com>
2022-06-28 17:54:09 +0900

    2022 06 28 17:55 구동확인

--- tf2/hand_writing2.py
+++ tf2/hand_writing2.py
@@ -41,6 +41,7 @@ class Solution(tf.keras.Model):
         # 모델 학습
         hist = model.fit(self.train_ds, validation_data=self.val_ds, epochs=10)
 
+
         # 모델 평가
         print('모델 평가')
         model.evaluate(self.x_test, self.y_test)
